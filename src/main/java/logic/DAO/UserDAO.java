acd6c3aca5563a17c22c7439e3b89adbfaf8f199
AriannaG0 <150864012+AriannaG0@users.noreply.github.com>
2026-04-17 18:17:09 +0200

    added getUserbyID and registerUser(not completed)

--- src/main/java/logic/DAO/UserDAO.java
+++ src/main/java/logic/DAO/UserDAO.java
@@ -12,23 +12,38 @@ import java.util.Map;
 import java.util.logging.Level;
 
 //RICORDA CHE DEVI INSERIRE IL LOGIN CON GOOGLE E CAPIRE COME FARE
-// inserire metodo getUserbyIDandUsername e metodo RegisterUser, metodo getUsernamebyID
-// serve un metodo che aggiorni lo stato delle richieste?
+
+
 
 public class UserDAO {
 
-    public Model_User verifyLogin(String username, String password) throws SQLException {
+    public Model_User verifyLogin(String username, String password, boolean isGoogleLogin) throws SQLException {
         Model_User user = null; // restuito nel caso l'utente non esistesse/i parametri fossero errati -> gestione dal controller
 
-        try (Connection conn = SingletonDBSession.getInstance().startConnection()) {
-            PreparedStatement pstatement = conn.prepareStatement("SELECT iduser, username, password, user_type FROM user_data WHERE username = ? AND password = ?");
-            pstatement.setString(1, username);
-            pstatement.setString(2, password);
-            user = getLoggedUser(pstatement);
+        if(!isGoogleLogin) {
+            // Accesso senza Google
+            try (Connection conn = SingletonDBSession.getInstance().startConnection()) {
+                PreparedStatement pstatement = conn.prepareStatement("SELECT iduser, username, password, user_type FROM user_data WHERE username = ? AND password = ?");
+                pstatement.setString(1, username);
+                pstatement.setString(2, password);
+                user = getLoggedUser(pstatement);
 
-        }finally{
-            SingletonDBSession.getInstance().closeConnection();
+            } finally {
+                SingletonDBSession.getInstance().closeConnection();
+            }
+        }else{
+            // Accesso con Google
+            try (Connection conn = SingletonDBSession.getInstance().startConnection()) {
+                PreparedStatement pstatement = conn.prepareStatement("SELECT iduser, username, password, user_type FROM user_data WHERE username = ? AND password = ?");
+                pstatement.setString(1, username);
+                user = getLoggedUser(pstatement);
+            }catch(SQLException e){e.printStackTrace();
+            }finally{
+                SingletonDBSession.getInstance().closeConnection();
+            }
+
         }
+
         return user;
     }
 
@@ -51,6 +66,42 @@ public class UserDAO {
         }
         return user;
     }
+    public int getUserIDbyUsername(int user_id, String username) throws SQLException {
+
+        try (Connection conn = SingletonDBSession.getInstance().startConnection()) {
+            PreparedStatement pstat = conn.prepareStatement("SELECT iduser FROM user_data WHERE username = ? ");
+            pstat.setString(1, username); //siamo sicuri sia 1?
+            try (ResultSet rs = pstat.executeQuery()) {
+                if (rs.next()) {
+                    user_id = rs.getInt(1);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            SingletonDBSession.getInstance().closeConnection();
+        }
+        return user_id;
+    }
+
+    //metodo per registrare l'utente
+public void registerUser(Model_User user) throws SQLException{
+        try(Connection conn = SingletonDBSession.getInstance().startConnection()) {
+            PreparedStatement pstat = conn.prepareStatement("INSERT INTO user_data (iduser, username, password, user_type) VALUES(NULL, ?, ?, ?)");
+            pstat.setString(1, user.getUsername());
+            pstat.setString(2, user.getPassword());
+            pstat.setString(3, user.getUsertype().toString()); //nel database è salvato come ENUM, potrebbe dare problemi
+            pstat.execute();
+        }catch (SQLException e){
+            //qualcosa. C'è il caso in cui l'username sia già stato preso
+
+        }finally{
+            SingletonDBSession.getInstance().closeConnection();
+        }
 }
+}
+
+
+
 
 
