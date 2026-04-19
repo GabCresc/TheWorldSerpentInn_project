b7b1f0b8033c2fe593931496b9e820b590eacf44
IvanCresc <154003551+IvanCresc@users.noreply.github.com>
2026-04-19 18:07:12 +0200

    Add files via upload

---
+++ src/main/java/logic/model/Local_Notification.java
@@ -0,0 +1,60 @@
+package com.worldserpentinn.model;
+
+public class Local_Notification {
+    private User_Types user_type;
+    private Notification_Types notification_type;
+    private String notified_name;
+    private String notifier_name;
+    private int notification_ID;
+    private int user_ID;
+
+
+    public Notification_Types getNotificationType(){
+        return notification_type;
+    }
+
+    public String getNotifier_Name(){
+        return notifier_name;
+    }
+
+    public String getNotified_Name(){
+        return notified_name;
+    }
+
+    public int getNotification_ID(){
+        return notification_ID;
+    }
+
+    public User_Types getUserType(){
+        return user_type;
+    }
+
+    public int getUser_ID(){
+        return user_ID;
+    }
+
+
+    public void setNotificationType(Notification_Types notification_type){
+        this.notification_type = notification_type;
+    }
+
+    public void setNotifier_Name(String notifier_name){
+        this.notifier_name = notifier_name;
+    }
+
+    public void setNotified_Name(String notified_name){
+        this.notified_name = notified_name;
+    }
+
+    public void setNotification_ID(int notification_ID){
+        this.notification_ID = notification_ID;
+    }
+
+    public void setUserType(User_Types user_type){
+        this.user_type = user_type;
+    }
+
+    public void setUser_ID(int user_ID){
+        this.user_ID=user_ID;
+    }
+}
---
+++ src/main/java/logic/model/Notification.java
@@ -0,0 +1,14 @@
+package com.worldserpentinn.model;
+
+public interface Notification {
+    String getNotificationType();
+    String getNotifier_Name();
+    String getNotified_Name();
+    Integer getNotification_ID();
+    User_Types getUserType();
+    void setNotificationType(Notification_Types notification_type);
+    void setNotifier_Name(String notifier_name);
+    void setNotified_Name(String notified_name);
+    void setNotification_ID(Integer notification_ID);
+    void setUserType(User_Types user_type);
+}
