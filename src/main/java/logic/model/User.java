6cbcb21e8ece9e96c862e632f33314c8ea83d8d4
IvanCresc <154003551+IvanCresc@users.noreply.github.com>
2026-04-21 14:59:43 +0200

    Update User.java

--- src/main/java/logic/model/User.java
+++ src/main/java/logic/model/User.java
@@ -4,6 +4,7 @@ public class User {
     private String username;
     private String password;
     public User_Types UserType;
+    public int User_ID;
 
     public void setUsername(String username) {
         this.username = username;
@@ -20,4 +21,17 @@ public class User {
     public String getPassword() {
         return password;
     }
+
+    public int getUserID(){
+        return User_ID
+    }
+    
+    public void setUserID(int User_ID){
+        this.User_ID=User_ID
+    }
+
+    public UserType getUserType(){
+        return UserType;
+    }
+    
 }
