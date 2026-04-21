a479377c573255af1f969704157548242a37d700
AriannaG0 <150864012+AriannaG0@users.noreply.github.com>
2026-04-21 16:04:11 +0200

    Update UserDAO.java

--- src/main/java/logic/DAO/UserDAO.java
+++ src/main/java/logic/DAO/UserDAO.java
@@ -1,29 +1,27 @@
 package logic.DAO;
 
-import logic.models.Model_User;
+import logic.model.User;
 import logic.utils.SingletonDBSession;
 import java.sql.SQLException;
-import logic.utils.User_Types;
+import logic.utils.enums.UserTypes;
 
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.logging.Level;
 
 //RICORDA CHE DEVI INSERIRE IL LOGIN CON GOOGLE E CAPIRE COME FARE
+
+// populate connplayers e conndm che crea una hash map?
 
 
 
 public class UserDAO {
 
-    public Model_User verifyLogin(String username, String password, boolean isGoogleLogin) throws SQLException {
-        Model_User user = null; // restuito nel caso l'utente non esistesse/i parametri fossero errati -> gestione dal controller
+    public User verifyLogin(String username, String password, boolean isGoogleLogin) throws SQLException {
+        User user = null; // restuito nel caso l'utente non esistesse/i parametri fossero errati -> gestione dal controller
 
         if(!isGoogleLogin) {
             // Accesso senza Google
             try (Connection conn = SingletonDBSession.getInstance().startConnection()) {
-                PreparedStatement pstatement = conn.prepareStatement("SELECT iduser, username, password, user_type FROM user_data WHERE username = ? AND password = ?");
+                PreparedStatement pstatement = conn.prepareStatement("SELECT userID, username, password, user_type FROM user_data WHERE username = ? AND password = ?");
                 pstatement.setString(1, username);
                 pstatement.setString(2, password);
                 user = getLoggedUser(pstatement);
@@ -34,7 +32,7 @@ public class UserDAO {
         }else{
             // Accesso con Google
             try (Connection conn = SingletonDBSession.getInstance().startConnection()) {
-                PreparedStatement pstatement = conn.prepareStatement("SELECT iduser, username, password, user_type FROM user_data WHERE username = ? AND password = ?");
+                PreparedStatement pstatement = conn.prepareStatement("SELECT userID, username, password, user_type FROM user_data WHERE username = ? AND password = ?");
                 pstatement.setString(1, username);
                 user = getLoggedUser(pstatement);
             }catch(SQLException e){e.printStackTrace();
@@ -48,15 +46,15 @@ public class UserDAO {
     }
 
     // questo metodo popola i campi di user nel qual caso l'utente fosse già registrato... se non lo è, restituisce null
-    public Model_User getLoggedUser(PreparedStatement pstatement) {
-       Model_User user = null;
+    public User getLoggedUser(PreparedStatement pstatement) {
+       User user = null;
         try (ResultSet rs = pstatement.executeQuery()) {
             if (rs.next()) {
-                user = new Model_User();
-                user.setID(rs.getInt("iduser"));
+                user = new User();
+                user.setID(rs.getInt("userID"));
                 user.setUsername(rs.getString("username"));
                 String UserTypSt = rs.getString("user_type").toUpperCase();
-                User_Types.valueOf(UserTypSt);
+                UserTypes.valueOf(UserTypSt);
             } else {
                 System.out.println("User does not exist"); //  teoricamente a questo punto dovrebbe essere invocata la registrazione
             }
@@ -69,7 +67,7 @@ public class UserDAO {
     public int getUserIDbyUsername(int user_id, String username) throws SQLException {
 
         try (Connection conn = SingletonDBSession.getInstance().startConnection()) {
-            PreparedStatement pstat = conn.prepareStatement("SELECT iduser FROM user_data WHERE username = ? ");
+            PreparedStatement pstat = conn.prepareStatement("SELECT userID FROM user_data WHERE username = ? ");
             pstat.setString(1, username); //siamo sicuri sia 1?
             try (ResultSet rs = pstat.executeQuery()) {
                 if (rs.next()) {
@@ -85,9 +83,9 @@ public class UserDAO {
     }
 
     //metodo per registrare l'utente
-public void registerUser(Model_User user) throws SQLException{
+public void registerUser(User user) throws SQLException{
         try(Connection conn = SingletonDBSession.getInstance().startConnection()) {
-            PreparedStatement pstat = conn.prepareStatement("INSERT INTO user_data (iduser, username, password, user_type) VALUES(NULL, ?, ?, ?)");
+            PreparedStatement pstat = conn.prepareStatement("INSERT INTO user_data (userID, username, password, user_type) VALUES(NULL, ?, ?, ?)");
             pstat.setString(1, user.getUsername());
             pstat.setString(2, user.getPassword());
             pstat.setString(3, user.getUsertype().toString()); //nel database è salvato come ENUM, potrebbe dare problemi
@@ -100,8 +98,3 @@ public void registerUser(Model_User user) throws SQLException{
         }
 }
 }
-
-
-
-
-
