2e348a651fdb48ac2c436deeb89eec31ddcdd46e
IvanCresc <154003551+IvanCresc@users.noreply.github.com>
2026-04-21 15:27:01 +0200

    Update Notification.java

--- src/main/java/logic/model/Notification.java
+++ src/main/java/logic/model/Notification.java
@@ -4,15 +4,17 @@ import logic.utils.enums.NotificationTypes;
 import logic.utils.enums.UserTypes;
 
 public interface Notification {
-    String getNotificationType();
+    Notification_Types getNotificationType();
     String getNotifier_Name();
     String getNotified_Name();
     int getNotification_ID();
     User_Types getUserType();
+    int getUser_ID();
 
     void setNotificationType(Notification_Types notification_type);
     void setNotifier_Name(String notifier_name);
     void setNotified_Name(String notified_name);
     void setNotification_ID(int notification_ID);
     void setUserType(User_Types user_type);
+    void setUser_ID(int user_ID)
 }
