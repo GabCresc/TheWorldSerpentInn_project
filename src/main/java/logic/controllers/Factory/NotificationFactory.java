9ca721cfa725a7e520be26b07cfa2e3f8ba81ffd
AriannaG0 <150864012+AriannaG0@users.noreply.github.com>
2026-04-26 18:54:06 +0200

    Update NotificationFactory.java

--- src/main/java/logic/controllers/Factory/NotificationFactory.java
+++ src/main/java/logic/controllers/Factory/NotificationFactory.java
@@ -7,22 +7,21 @@ import logic.utils.enums.NotificationTypes;
 import logic.utils.enums.Notification_Kind;
 import logic.utils.enums.UserTypes;
 
-
 public class NotificationFactory {
-    public Notification CreateLocalNotification(int notification_id, int notifier_id, int notified_id, NotificationTypes type, int campaign_id, UserTypes notifier_type, UserTypes notified_type) {
-        return new Local_Notification(notification_id, notifier_id, notified_id, type, campaign_id, notifier_type, notified_type);
+    public Notification CreateLocalNotification(int notification_id, int notifier_id, int notified_id, NotificationTypes type, int campaign_id) {
+        return new Local_Notification(notification_id, notifier_id, notified_id, type, campaign_id);
     }
 
-    public Notification CreateServerNotification(int notification_id, int notifier_id, int notified_id, NotificationTypes type, int campaign_id, UserTypes notifier_type, UserTypes notified_type) {
-        return new Server_Notification(notification_id, notifier_id, notified_id, type, campaign_id, notifier_type, notified_type);
+    public Notification CreateServerNotification(int notification_id, int notifier_id, int notified_id, NotificationTypes type, int campaign_id) {
+        return new Server_Notification(notification_id, notifier_id, notified_id, type, campaign_id);
     }
 
-    public Notification CreateNotification(int notification_id, int notifier_id, int notified_id, NotificationTypes type, int campaign_id, UserTypes notifier_type, UserTypes notified_type) {
+    public Notification CreateNotification(int notification_id, int notifier_id, int notified_id, NotificationTypes type, int campaign_id) {
         Notification_Kind kind = type.getKind();
         if (kind == Notification_Kind.LOCAL) {
-            return CreateLocalNotification(notification_id, notifier_id, notified_id, type, campaign_id, notifier_type, notified_type);
+            return CreateLocalNotification(notification_id, notifier_id, notified_id, type, campaign_id);
         } else {
-            return CreateServerNotification(notification_id, notifier_id, notified_id, type, campaign_id, notifier_type, notified_type);
+            return CreateServerNotification(notification_id, notifier_id, notified_id, type, campaign_id);
         }
     }
 }
