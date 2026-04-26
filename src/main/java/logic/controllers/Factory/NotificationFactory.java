c8c1a24afd0815257d64ebe7b95ac2f6771d28af
AriannaG0 <150864012+AriannaG0@users.noreply.github.com>
2026-04-26 10:52:01 +0200

    added usertypes enums and deleted log

--- src/main/java/logic/controllers/Factory/NotificationFactory.java
+++ src/main/java/logic/controllers/Factory/NotificationFactory.java
@@ -5,10 +5,9 @@ import logic.model.Local_Notification;
 import logic.model.Server_Notification;
 import logic.utils.enums.NotificationTypes;
 import logic.utils.enums.Notification_Kind;
-import lombok.extern.java.Log;
+import logic.utils.enums.UserTypes;
 
 
-@Log
 public class NotificationFactory {
     public Notification CreateLocalNotification(int notification_id, int notifier_id, int notified_id, NotificationTypes type, int campaign_id, UserTypes notifier_type, UserTypes notified_type) {
         return new Local_Notification(notification_id, notifier_id, notified_id, type, campaign_id, notifier_type, notified_type);
